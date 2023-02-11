49711dd9e61cd5abb4495559ad4a5fd7b72516c2
hitman077 <ntw3840@gmail.com>
2023-02-11 13:36:31 +0700

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,35 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    ssr: false,
+    app: {
+        head: {
+          htmlAttrs: {
+            lang: 'en'
+          },
+          bodyAttrs: {
+            class: 'demo'
+          },
+          charset: 'utf-8',
+          titleTemplate: '%s - Nuxt 3 Vuetify',
+          meta: [
+            { 
+              name: 'viewport', 
+              content: 'width=device-width, initial-scale=1, maximum-scale=5'
+            },
+            {
+              name: 'author',
+              content: "IT Genius Engineering Ltd., Thailand"
+            },
+          ]
+        }
+    },
+    build: {
+      transpile: ["vuetify"],
+    },
+    css: ["@/assets/scss/style.scss"],
+    vite: {
+      define: {
+        "process.env.DEBUG": false,
+      },
+    },
+  });
